2e161b0b9896252ec4b162564c1eff48cdee9fdb
Colton <watkincw@gmail.com>
2021-06-15 23:21:04 -0500

    added issue #6

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,50 @@
 ////// Assignment code here
 
-
-//// Generate Password
-
-// Variables for generatePassword()
 const charCatagories = {
     upCase: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
     'P','Q','R','S','T','U','V','W','X','Y','Z'],
     lowCase: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
     'p','q','r','s','t','u','v','w','x','y','z'],
     number: ['0','1','2','3','4','5','6','7','8','9'],
-    symbol: ['£','$','&','(',')','*','+','[',']','@','#','^','-','_','!','?'],
+    symbol: ['$','&','(',')','*','+','[',']','@','#','^','-','_','!','?'],
 }
+// picking 4 random characters, one value from each key in the charCatagories object
+let randomChar = {
+    rUpper: charCatagories.upCase[Math.floor(Math.random() * charCatagories.upCase.length)],
+    rLower: charCatagories.lowCase[Math.floor(Math.random() * charCatagories.lowCase.length)],
+    rNum: charCatagories.number[Math.floor(Math.random() * charCatagories.number.length)],
+    rSym: charCatagories.symbol[Math.floor(Math.random() * charCatagories.symbol.length)]
+}
+
 
 
 function passLength () {
     // prompt returns a string, we need it to be a number so we parseInt
     let answer =  parseInt(prompt("How long do you want your password to me? min: 8 max: 128"))
-    //we just make sure its a good number -- this is the bad case
-    if (answer < 8 || answer > 128) {
-        alert('hey dumb dumb its gotta be a number and between 8 and 128')
-        passLength()
+    // we just make sure its a good number -- this is the bad case
+    if (answer < 8 || answer > 128 ) {
+        alert("Hey dumb dumb, it's gotta be a number and between 8 and 128")
+        passLength();
     } else {
         //they answered right-- they are smarties
         return answer;
     }
 }
 
-function characterCatagories() {
+function chooseCharacterTypes() { 
     let availableCharacters = ''
 
     let wantUppers = confirm('Do you want uppercase? (A-Z)') //wantUppers = true/false (confirm returns boolean)
     let wantLowers = confirm('Do you want lowercase? (a-z)') 
     let wantNumbers = confirm('Do you want Numbers? "0-9"')
-    let wantSymbols = confirm('Do you want Special Characters? "£$&()*+[]@#^-_!?"')
+    let wantSymbols = confirm('Do you want Special Characters? "$&()*+[]@#^-_!?"')
 
     //they answer no to every character set
     if (!wantUppers && !wantLowers && !wantSymbols && !wantNumbers) { 
         alert('Hey dumb dumb, we need characters to chose from. How do we make a password with no charactrs?');
-        characterCatagories();
+        chooseCharacterTypes();
     } else {
-        //they answered yes to at least one thing
+        //they answered yes to at least one prompt
         if (wantUppers) {
             // concatenating the string'd version of the array set -BELOW COULD ALSO BE DESCRIBED AS- join('') takes the array'd [values] and turns it into 1 string, as apposed to how its currently seperated into individual strings for each letter/number/symbol. !!!You could avoid using this if you set up your arrays more effienctly the first time, dummmy...
             availableCharacters += charCatagories.upCase.join('')
@@ -54,11 +58,12 @@ function characterCatagories() {
         if (wantSymbols) { 
             availableCharacters += charCatagories.symbol.join('')
         }
-
-        return availableCharacters
+        // displays in the console which characters will be accepted, using boolean values, in the order they were asked
+        console.log(wantUppers, wantLowers, wantNumbers, wantSymbols);
+        // assigns chooseCharacterTypes ALL CHARACTERS from ALL CHOSEN TYPES to availableCharacters
+        return availableCharacters;
     }
 }
-
 
 function generatePassword() {
     let password = ''
@@ -66,16 +71,25 @@ function generatePassword() {
     // length of password
     let desiredLength = passLength();  
     // What combination of characters do you want in your password?
-    let avaiableCharacterSetAsString = characterCatagories();
+    let chosenCharacters = chooseCharacterTypes();
 
-    // use the above data to create a password with a for
+    // use the above data to create a password with a for loop
     // manipulate to create actual password
-    for (var i = 0; i <= desiredLength; i++) { 
-        
+
+    for (var i = 0; i < desiredLength; i++) { 
+////////// What I am trying to do is take ONE value from ONE of the keys in randomChar, randomly, and return that as the first character. Then loop until i = desiredLength //////////
+////////// ALL possible character types are chosen already in chooseCharacterTypes() and assigned to the chosenCharacters var defined above in generatePassword() //////////
+////////// SOOOOOOOOOOOOOOOO //////////
+////////// I have to get a random value from the chosenCharacters var and add it to i in this for loop //////////
+
+        // select one random value from chosenCharacters and add it to password
+        password += chosenCharacters[(Math.floor(Math.random() * chosenCharacters.length))]
+
+
+        // console.log(password);
         return password;
     }
 }
-
 
 
 // Get references to the #generate element
@@ -83,7 +97,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() { 
-    // storing something as password. it is the result of the generate password function
+    // storing the result of generatePassword()
     let password = generatePassword();
     let passwordText = document.querySelector("#password");
     //we put the generated password on the page
@@ -92,3 +106,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// // picking one random value from randomChar (FOR LOOP IN generatePassword())
+// var randomCharacter = function (randomChar) { 
+//     var keys = Object.keys(randomChar);
+//     return randomChar[keys[keys.length * Math.random() << 0]];
+// }
+// randomCharacter();
